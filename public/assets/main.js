59166a7e84b879755a29bb8319aff505c09a3e09
Luan Vu <luan@eigyo.tech>
2025-12-19 09:31:02 +0700

    Update tax calculation inputs and labels for new regulations

--- public/assets/main.js
+++ public/assets/main.js
@@ -34,9 +34,11 @@ document.querySelector("input[name='taxDone']").oninput = function () {
     }
     calculate()
 }
-document.querySelector("input[name='newTaxReg']").onchange = function () {
-    calculate()
-}
+document.querySelectorAll("input[name='newTaxBand'], input[name='newTaxReg']").forEach(function(el) {
+    el.onchange = function() {
+        calculate();
+    };
+});
 
 function readGroup(group) {
     let readDigit = [" Không", " Một", " Hai", " Ba", " Bốn", " Năm", " Sáu", " Bảy", " Tám", " Chín"];
@@ -57,24 +59,39 @@ function readGroup(group) {
 }
 
 function calculate() {
+    document.querySelectorAll("input[name='newTaxBand'], input[name='newTaxReg']").forEach(function(el) {
+        el.onchange = function() {};
+    });
     let useNewTaxReg = document.querySelector("input[name='newTaxReg']").checked;
-    
+    let useNewTaxBand = document.querySelector("input[name='newTaxBand']").checked;
     let TAX_RATES, TAX_BANDS, INDIVIDUAL_DEDUCTION, DEPENDENT_DEDUCTION;
-    
+    TAX_RATES = [0.05, 0.1, 0.15, 0.2, 0.25, 0.3, 0.35];
+    TAX_BANDS = [5_000_000, 10_000_000, 18_000_000, 32_000_000, 52_000_000, 80_000_000, Infinity];
+    // If useNewTaxBand is checked, also checked useNewTaxReg
     if (useNewTaxReg) {
-        // New tax regulations from 2026
-        TAX_RATES = [0.05, 0.1, 0.2, 0.3, 0.35];
-        TAX_BANDS = [10_000_000, 30_000_000, 60_000_000, 100_000_000, Infinity];
         INDIVIDUAL_DEDUCTION = 15_500_000;
         DEPENDENT_DEDUCTION = 6_200_000;
     } else {
         // Current tax regulations
-        TAX_RATES = [0.05, 0.1, 0.15, 0.2, 0.25, 0.3, 0.35];
-        TAX_BANDS = [5_000_000, 10_000_000, 18_000_000, 32_000_000, 52_000_000, 80_000_000, Infinity];
         INDIVIDUAL_DEDUCTION = 11_000_000;
         DEPENDENT_DEDUCTION = 4_400_000;
+        useNewTaxBand = false;
+        document.querySelector("input[name='newTaxBand']").checked = false;
     }
-
+    if (useNewTaxBand) {
+        useNewTaxReg = true;
+        document.querySelector("input[name='newTaxReg']").checked = true;
+        TAX_RATES = [0.05, 0.1, 0.2, 0.3, 0.35];
+        TAX_BANDS = [10_000_000, 30_000_000, 60_000_000, 100_000_000, Infinity];
+    } else {
+        TAX_RATES = [0.05, 0.1, 0.15, 0.2, 0.25, 0.3, 0.35];
+        TAX_BANDS = [5_000_000, 10_000_000, 18_000_000, 32_000_000, 52_000_000, 80_000_000, Infinity];
+    }
+    document.querySelectorAll("input[name='newTaxBand'], input[name='newTaxReg']").forEach(function(el) {
+        el.onchange = function() {
+            calculate();
+        };
+    });
     let income = document.querySelector("input[name='income']").value
     let npt = document.querySelector("input[name='npt']").value
     let taxDone = document.querySelector("input[name='taxDone']").value
